0723a9a63b89b0a3e9b265d3f1ef661cbbbee1cf
Jawad Rezaei <107846753+jawadRezaei10@users.noreply.github.com>
2023-05-13 11:25:49 +0430

    dice functionalities added

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,65 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
           title: Text('Dice'),
         ),
-        body: MyApp(),
+        body: const DicePage(),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftCountBtn = 1;
+  int rightCountBtn = 1;
+
+  void callBack() {
+    setState(() {
+      leftCountBtn = Random().nextInt(6) + 1;
+      rightCountBtn = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                callBack();
+              },
+              child: Image.asset('images/dice$leftCountBtn.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  callBack();
+                });
+              },
+              child: Image.asset("images/dice$rightCountBtn.png"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
